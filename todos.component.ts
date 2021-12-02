5668573ae64b1139cc9c79466e63bb1108796f9e
Giorgi Dogadze <58903449+giorgi-dogadze@users.noreply.github.com>
2021-12-02 19:15:01 +0000

    feat/ create todos.component.ts file

---
+++ todos.component.ts
@@ -0,0 +1,12 @@
+@Component({
+  selector: 'todos-cmp',
+  template: `
+    <div *ngFor="let t of todos|async">
+        {{t.description}}
+    </div>
+  `
+})
+class TodosComponent {
+  todos: Observable<Todo[]> = this.store.pipe(select('todos'));
+  constructor(private store: Store<AppState>) {}
+}
